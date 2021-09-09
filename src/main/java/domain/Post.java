d817629b2c60651477c8b0a5dc2e68f84ea3dd79
Timofey Chernasov <timofeychernasov@gmail.com>
2021-09-09 12:23:11 +0300

    Add data

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,19 @@
+package domain;
+
+public class Post {
+    private String id;
+    private String imageLogo;
+    private String userName;
+    private String dateTimeOfPublic;
+    private String post;
+    private String videoPostUrl;
+    private String imagePost;
+    private String videoPost;
+    private boolean favorite;
+    private int like;
+    private int repost;
+    private int view;
+    private String comment;
+
+    // + get/set на все поля
+}
